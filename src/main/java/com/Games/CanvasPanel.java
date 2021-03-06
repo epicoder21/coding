103c5cb16c8e30f2adb8796b20bd682acc406869
epicoder21 <76849959+epicoder21@users.noreply.github.com>
2021-03-06 16:46:30 -0600

    Create CanvasPanel.java

--- src/main/java/com/Games/CanvasPanel.java
+++ src/main/java/com/Games/CanvasPanel.java
@@ -1,4 +1,4 @@
-package com.Games;
+package stuff;
 
 import javax.imageio.ImageIO;
 import javax.sound.sampled.*;
@@ -15,11 +15,15 @@ public class CanvasPanel extends JPanel {
     BufferedImage East = null;
     BufferedImage West = null;
     BufferedImage img = null;
+    BufferedImage timesTwo = null;
+    BufferedImage watermelon = null;
     boolean canGoSouth = true;
     boolean canGoNorth = true;
     boolean canGoWest = true;
     boolean canGoEast = true;
     boolean removeTail = true;
+    boolean isthereMulti = false;
+    boolean eatenMulti = false;
     int score = 0;
     int[][] data = new int[20][20];
     int direct = 0;//zero is south, 1 is east, 2 is west 3 is north
@@ -35,6 +39,10 @@ public class CanvasPanel extends JPanel {
     public CanvasPanel() {
         try {
             img = ImageIO.read(CanvasPanel.class.getClassLoader().getResource("apple.png").openStream());
+
+            timesTwo = ImageIO.read(CanvasPanel.class.getClassLoader().getResource("timestwomultiplier.jpg").openStream());
+
+            watermelon = ImageIO.read(CanvasPanel.class.getClassLoader().getResource("watermelon.jpg").openStream());
 
             North = ImageIO.read(CanvasPanel.class.getClassLoader().getResource("north.png").openStream());
 
@@ -62,7 +70,9 @@ public class CanvasPanel extends JPanel {
         data[10][9] = 1;
         data[10][8] = 1;
         data[10][7] = 1;
-        this.randomApple(10);
+        this.randomApple(4);
+        this.randomMelon(1);
+        this.randomMultiplier(1);
         score = 0;
     }
 
@@ -81,20 +91,70 @@ public class CanvasPanel extends JPanel {
 
 
     }
-    public void randomApple(int numApp) {
-       for (int a = 0; a < numApp; a++) {
-           int appX = (int)(Math.random()*gridLength);
-           int appY = (int)(Math.random()*gridLength);
-           data[appX][appY] = 3;
-       }
+    public void playsoundDeath(){
+        try {
+            AudioInputStream audioInputOOf = AudioSystem.getAudioInputStream(
+                    Objects.requireNonNull(this.getClass().getClassLoader().getResource("classic_hurt.wav")).openStream());
+            Clip oof;
+            oof = AudioSystem.getClip();
+            oof.open(audioInputOOf);
+            oof.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
 
     }
+
+    Font big;
+    public void randomApple(int numApp) {
+        for (int a = 0; a < numApp; a++) {
+            int appX = (int)(Math.random()*gridLength);
+            int appY = (int)(Math.random()*gridLength);
+            if (data[appX][appY]==1 || data[appX][appY]==3 || data[appX][appY] == 2 || data[appX][appY] == 4){
+                data[(int)(Math.random() * gridLength)][(int)(Math.random() * gridLength)]=3;
+            } else {
+                data[appX][appY] = 3;
+            }
+        }
+
+    }
+
+    public void randomMelon(int numMelon) {
+        for (int m = 0; m < numMelon; m++) {
+            int melX = (int)(Math.random()*gridLength);
+            int melY = (int)(Math.random()*gridLength);
+            if (data[melX][melY]==1 || data[melX][melY]==3 || data[melX][melY] == 2 || data[melX][melY] == 4){
+                data[(int)(Math.random() * (gridLength))][(int)(Math.random() * (gridLength))]=2;
+            } else {
+                data[melX][melY] = 2;
+            }
+        }
+
+    }
+
+    public void randomMultiplier(int numMulti) {
+        for (int M = 0; M < numMulti; M++) {
+            int randomizer = (int) (Math.random()*100);
+            if (randomizer <= 15) {
+                int mulX = (int)(Math.random()*gridLength);
+                int mulY = (int)(Math.random()*gridLength);
+                isthereMulti = true;
+                if (data[mulX][mulY]==1 || data[mulX][mulY]==3 || data[mulX][mulY] == 2 || data[mulX][mulY] == 4){
+                    data[(int)(Math.random() * (gridLength))][(int)(Math.random() * (gridLength))]=4;
+                } else {
+                    data[mulX][mulY] = 4;
+                }
+            }
+        }
+    }
+
     Font last;
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         last = g.getFont();
-
+//1 = snake body, 0 = empty cell, 3 = apple, 2 = watermelon
         for (int i = 0; i < data.length; i++) {
             for(int j = 0; j < data.length; j++) {
                 if (data[i][j] == 1) {
@@ -126,11 +186,24 @@ public class CanvasPanel extends JPanel {
 
                     g.drawImage(img, i * cellWidth, j
                             * cellWidth, cellWidth, cellWidth, null);
+                } else if(data[i][j] == 2){
+                    g.drawImage(watermelon, i * (cellWidth), j
+                            * (cellWidth), cellWidth, cellWidth, null);
+
+                    g.setColor(Color.black);
+                    g.drawRect(i*cellWidth, j*cellWidth, cellWidth, cellWidth);
+                }else if(data[i][j] == 4){
+                    g.drawImage(timesTwo, i * (cellWidth), j
+                            * (cellWidth), cellWidth, cellWidth, null);
+
+                    g.setColor(Color.black);
+                    g.drawRect(i*cellWidth, j*cellWidth, cellWidth, cellWidth);
                 }
             }
         }
         if (MyMain.onGame == 0) {
             g.setFont(new Font("TimesRoman", Font.BOLD, 30));
+            big = g.getFont();
             g.drawString("Press Space to start", 350, 375);
             g.setFont(last);
         } else if (MyMain.onGame == 2 ){
@@ -141,8 +214,40 @@ public class CanvasPanel extends JPanel {
             g.drawString("Highscore:" + highscore, 350, 315);
             g.setFont(last);
         }
-        g.drawString("Your score =" + score, 10, 740);
-        g.drawString("Highscore =" + highscore, 10, 780);
+        g.setFont(new Font("TimesRoman", Font.BOLD, 30));
+        g.drawString("Score = " + score, 495, 750);
+        g.setFont(new Font("TimesRoman", Font.BOLD, 20));
+        g.drawString("Highscore:" + highscore, 495, 800);
+        g.setFont(last);
+
+        if (MyMain.onGame == 0 || MyMain.onGame == 2) {
+            g.setFont(new Font("TimesRoman", Font.BOLD, 30));
+            big = g.getFont();
+            g.drawString("Speed = 0", 890, 700);
+            g.setFont(last);
+        } else if (MyMain.onGame == 1 ){
+            g.setFont(new Font("TimesRoman", Font.BOLD, 30));
+            big = g.getFont();
+            g.drawString("Speed = " + MyMain.speed, 890, 700);
+            g.setFont(last);
+        }
+        //difficulty
+        if (MyMain.difficulty == 0) {
+            g.setFont(new Font("TimesRoman", Font.BOLD, 45));
+            big = g.getFont();
+            g.drawString("Easy Mode", 800, 650);
+            g.setFont(last);
+        } else if (MyMain.difficulty == 1 ){
+            g.setFont(new Font("TimesRoman", Font.BOLD, 45));
+            big = g.getFont();
+            g.drawString("Medium Mode" , 800, 650);
+            g.setFont(last);
+        } else if (MyMain.difficulty == 2)
+            g.setFont(new Font("TimesRoman", Font.BOLD, 45));
+        big = g.getFont();
+        g.drawString("Hard Mode" , 800, 650);
+        g.setFont(last);
+
     }
 
     public void movegame() {
@@ -179,9 +284,10 @@ public class CanvasPanel extends JPanel {
         }
         if (CurrY == 20 || CurrX == 20 || CurrX == -1 || CurrY == -1) {
             MyMain.onGame = 2;
+            playsoundDeath();
             return;
         }
-         removeTail = true;
+        removeTail = true;
 
         if(data[CurrX][CurrY] == 3) {
             score += 1;
@@ -189,12 +295,51 @@ public class CanvasPanel extends JPanel {
                 highscore = score;
             }
             this.randomApple(1);
+            if (isthereMulti == false) {
+                this.randomMultiplier(1);
+            }
             removeTail = false;
             playsound();
         }
+        if(data[CurrX][CurrY] == 2) {
+            score += 3;
+            if (score > highscore) {
+                highscore = score;
+            }
+            this.randomMelon(1);
+            if (isthereMulti == false) {
+                this.randomMultiplier(1);
+            }
+            removeTail = false;
+            playsound();
+        }
+        if(data[CurrX][CurrY] == 4) {
+            eatenMulti = true;
+        }
+        int spawnExtraApp;
+//        if (MyMain.onGame == 1) {
+//            for (int boardLength = 0; boardLength <= 20; boardLength++) {
+//                for (int boardLength2 = 0; boardLength2 <= 20; boardLength2++) {
+//                    if (data[boardLength][boardLength2] == 2) {
+//                        MyMain.waterCount = MyMain.waterCount + 1;
+//                    }
+//                    if (data[boardLength][boardLength2] == 3) {
+//                        MyMain.appCount = MyMain.appCount + 1;
+//                    }
+//                }
+//            }
+//            if (MyMain.waterCount != 1) {
+//             randomMelon(1);
+//            }
+//            if (MyMain.appCount != 4) {
+//                spawnExtraApp = 4 - MyMain.appCount;
+//                randomMelon(spawnExtraApp);
+//            }
+//        }
 
         if (data[CurrX][CurrY] == 1) {
             MyMain.onGame = 2;
+            playsoundDeath();
             return;
         }
 
